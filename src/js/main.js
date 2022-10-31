704ec37130aa0e9665c9065a3388ac07e01ce2e3
Adam Stuborn <adam_stuborn@hotmail.com>
2022-10-31 23:31:53 +0100

    m

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,95 @@
 let addButton = document.getElementById("addButton");
 let userInput = document.getElementById("userInput");
 
-let toDo = ["Städa", "Tvätta", "Handla", "Mata katten"];
+const toDos = [
+  { title: "Städa", isDone: false },
+  { title: "Tvätta", isDone: false },
+  { title: "Handla", isDone: false },
+  { title: "Mata katten", isDone: false },
+];
+const doneToDos = [];
 
-let info = document.createElement("ul");
+let toDoList = document.getElementById("toDo-list");
+let doneList = document.getElementById("done-list");
+let toDoContainer = document.getElementById("unchecked");
+let doneContainer = document.getElementById("checked");
 
-for (let i = 0; i < toDo.length; i++) {
-  console.log(toDo[i]);
+addButton.addEventListener("click", myAddButton);
 
-  let container = document.getElementById("container");
+renderList();
 
-  let list = document.createElement("li");
-  let button = document.createElement("button");
-  let checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox");
+function renderList() {
+  toDoList.innerHTML = "";
+  doneList.innerHTML = "";
 
-  addButton.addEventListener("click", myAddButton);
-  button.addEventListener("click", clicker);
+  for (let i = 0; i < toDos.length; i++) {
+    console.log(toDos[i]);
+    let listItem = document.createElement("li");
 
-  list.innerHTML = toDo[i];
-  button.innerHTML = "Ta bort";
+    // THIS IS NEW
+    const wrapper = document.createElement("div");
+    const title = document.createElement("span");
 
-  document.body.appendChild(container);
-  container.appendChild(info);
-  info.appendChild(list);
-  list.appendChild(checkbox);
-  list.appendChild(button);
+    const checkbox = document.createElement("input");
+    let button = document.createElement("button");
+    // Create checkbox
+
+    checkbox.setAttribute("type", "checkbox");
+
+    // Set checkbox checked if todo is done
+    checkbox.checked = toDos[i].isDone; // true | false
+
+    // Add an eventlistener to the checkbox that will fire on any "change"
+    checkbox.addEventListener("change", (event) => {
+      console.log("is Checked:", event.target.checked);
+      // Update the todos isDone value to checkbox value. I.e. if checked = todo is done.
+      toDos[i].isDone = event.target.checked;
+      renderList();
+    });
+
+    button.addEventListener("click", () => removeToDo(i));
+
+    title.innerText = toDos[i].title;
+    button.innerHTML = "X";
+
+    // <listItem>
+    //   <wrapper>
+    //     <checkbox></checkbox>
+    //     <title></title>
+    //   </wrapper>
+    //   // Här blir det space
+    //   <button></button>
+    // </listItem>
+
+    // THIS IS NEW
+    wrapper.append(checkbox, title);
+
+    listItem.appendChild(wrapper);
+    listItem.appendChild(button);
+
+    if (toDos[i].isDone) {
+      doneList.appendChild(listItem);
+    } else {
+      toDoList.appendChild(listItem);
+    }
+  }
 }
 
-function clicker() {
-  //ta bort li taggen
+function removeToDo(i) {
+  //tar bort ut listan
   console.log("click");
+  toDos.splice(i, 1);
+  renderList();
 }
 
 function myAddButton() {
-  //adda det som skrivits i inputen till listan
-  toDo.push(userInput.value);
-  console.log(userInput.value);
+  // Om man inte skriver något så kan man inte lägga till
+  if (userInput.value !== "") {
+    const todoToAdd = { title: userInput.value, isDone: false };
+
+    toDos.push(todoToAdd);
+    userInput.value = "";
+    renderList();
+    console.log(toDos);
+  }
 }
